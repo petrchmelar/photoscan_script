8d3c709a759c1bf572041f054dd9d4d8a1680acc
petrchmelar179 <petrchmelar179@gmail.com>
2017-04-05 09:48:30 +0200

    WIP: Added methods for config loading into the Configuration class

--- PhotoScanConfig.py
+++ PhotoScanConfig.py
@@ -1,61 +1,179 @@
 from PhotoScan import *
-import argparse
-import glob
 import os
 
-from configparser import ConfigParser, NoOptionError, NoSectionError
+from configparser import ConfigParser, NoOptionError
 
-class PSConfigurator:
-	def __init__(self, config_file_path):
-		self.cfg_parser = ConfigParser()
-		self.cfg_parser.read(config_file_path)
 
-		Alignment_options = {
-			"accuracy" : {
-			"LowestAccuracy" 		: PhotoScan.LowestAccuracy,
-			"LowAccuracy" 	 		: PhotoScan.LowAccuracy,
-			"MediumAccuracy" 		: PhotoScan.MediumAccuracy,
-			"HighAccuracy" 			: PhotoScan.HighAccuracy,
-			"HighestAccuracy" 		: PhotoScan.HighestAccuracy,
-			},
-			"preselection" : {
-			"NoPreselection" 		: PhotoScan.NoPreselection,
-			"GenericPreselection"	: PhotoScan.GenericPreselection,
-			"ReferencePreselection"	: PhotoScan.ReferencePreselection,
-			}
-		}
+class Configuration:
+    def __init__(self, config_file_path=None):
 
-		Dense_Cloud_options = {
-			"quality" : {
-			"UltraHigh"				: PhotoScan.UltraQuality,
-			"High"					: PhotoScan.HighQuality,
-			"Medium"				: PhotoScan.MediumQuality,
-			"Low"					: PhotoScan.LowQuality,
-			"Lowest"				: PhotoScan.LowestQuality,
-			},
-			"depth_filtering": {
-			"Disabled"				: PhotoScan.NoFiltering,
-			"Mild"					: PhotoScan.MildFiltering,
-			"Moderate"				: PhotoScan.moderateFiltering,
-			"Aggressive"			: PhotoScan.AggressiveFiltering,
-			}
-		}
+        # default configuration
+        # GENERAL section (this values  need to be loaded from the config file...)
+        self.project_name = ""
+        self.working_directory = ""
+        self.project_directory = ""
+        self.log_path = ""
+        self.images_directory = ""
+        self.mask_path = ""
 
-		#default configuration
-		self.accuracy = Alignment_options["accuracy"]["MediumAccuracy"]
-		self.photos_alignment_preselection = Alignment_options["preselection"]["NoPreselection"]
-		self.photos_alignment_generic_preselection = True
-		self.photos_alignment_key_point_limit = 40000
-		self.photos_alignment_tie_point_limit = 4000
+        # photos alignment section (this configurations are optional)
+        # default values could be set by calling LoadDefaultConfig method
+        self.photos_alignment_accuracy = None
+        self.photos_alignment_preselection = None
+        self.photos_alignment_generic_preselection = None
+        self.photos_alignment_key_point_limit = None
+        self.photos_alignment_tie_point_limit = None
 
-		self.dense_cloud_quality = Dense_Cloud_options["quality"]["Medium"]
-		self.dense_cloud_filtering = Dense_Cloud_options["depth_filtering"]["Disabled"]
+        self.LoadDefaultConfig()
+        # load config file if path is available
+        if config_file_path is not None:
+            self.LoadConfigFile(config_file_path)
 
-	def ConfigureGeneral(self):
-		pass
+    def ConfigureGeneral(self):
+        pass
 
-	def ConfigureAlignment(self):
-		pass
+    def ConfigureAlignment(self):
+        pass
 
-	def ConfigureDenseCloud(self):
-		pass
+    def ConfigureDenseCloud(self):
+        pass
+
+    def LoadConfigFile(self, config_file_path):
+        # load config file section
+        # GENERAL section
+        cfg_parser = ConfigParser()
+        cfg_parser.read(config_file_path)
+        print("Loading config file...")
+        project_name = cfg_parser.get('general', 'project_name')
+        print("Project name configuration successfully loaded: {}".format(project_name))
+
+        working_directory = cfg_parser.get('general', 'working_directory')
+        if not os.path.exists(working_directory):
+            raise IOError("Path {} does not exist".format(working_directory))
+        print("Working directory configuration successfully loaded: {}".format(working_directory))
+
+        project_directory = os.path.join(working_directory, cfg_parser.get('general', 'project_directory'))
+        if not os.path.exists(project_directory):
+            print("Project directory {} doesn't exist. Creating new one...".format(project_directory))
+            os.mkdir(project_directory)
+        print("Project directory configuration successfully loaded: {}".format(project_directory))
+
+        log_path = os.path.join(working_directory, cfg_parser.get('general', 'log_path'))
+        if not os.path.exists(log_path):
+            raise IOError("Path {} does not exist".format(log_path))
+        print("Logs path configuration successfully loaded: {}".format(log_path))
+
+        images_directory = os.path.join(working_directory, cfg_parser.get('general', 'images_directory'))
+        if not os.path.exists(images_directory):
+            raise IOError("Path {} does not exist".format(images_directory))
+        print("Photos directory configuration successfully loaded: {}".format(images_directory))
+
+        mask_path = os.path.join(working_directory, cfg_parser.get('general', 'mask_path'))
+        if not os.path.exists(mask_path):
+            raise IOError("Path {} does not exist".format(mask_path))
+        print("Mask path configuration successfully loaded: {}".format(mask_path))
+
+
+        # photos alignment section
+        try:
+            accuracy = cfg_parser.get('photos_alignment', 'accuracy')
+            if accuracy == "LowestAccuracy":
+                photos_alignment_accuracy = PhotoScan.LowestAccuracy
+            elif accuracy == "LowAccuracy":
+                photos_alignment_accuracy = PhotoScan.LowAccuracy
+            elif accuracy == "MediumAccuracy":
+                photos_alignment_accuracy = PhotoScan.MediumAccuracy
+            elif accuracy == "HighAccuracy":
+                photos_alignment_accuracy = PhotoScan.HighAccuracy
+            elif accuracy == "HighestAccuracy":
+                photos_alignment_accuracy = PhotoScan.HighestAccuracy
+            else:
+                photos_alignment_accuracy = PhotoScan.MediumAccuracy
+                print("Photos alignment accuracy option doesn't found in config file. Default setting will be used (MediumAccuracy).")
+        except NoOptionError:
+            photos_alignment_accuracy = PhotoScan.MediumAccuracy
+            print("Photos alignment accuracy option doesn't found in config file. Default setting will be used (MediumAccuracy).")
+        print("Photos alignment accuracy loaded: {}".format(str(photos_alignment_accuracy)))
+
+        try:
+            preselection = cfg_parser.get('photos_alignment', 'preselection')
+            if preselection == "NoPreselection":
+                photos_alignment_preselection = PhotoScan.NoPreselection
+            elif preselection == "GenericPreselection":
+                photos_alignment_preselection = PhotoScan.GenericPreselection
+            elif preselection == "ReferencePreselection":
+                photos_alignment_preselection = PhotoScan.ReferencePreselection
+            else:
+                photos_alignment_preselection = PhotoScan.NoPreselection
+                print(
+                    "Photos alignment preselection option doesn't found in config file. Default setting will be used (NoPreselection).")
+        except NoOptionError:
+            photos_alignment_preselection = PhotoScan.NoPreselection
+            print("Photos alignment preselection option doesn't found in config file. Default setting will be used (NoPreselection).")
+        print("Photos alignment preselection loaded: {}".format(str(photos_alignment_preselection)))
+
+        try:
+            generic_preselection = cfg_parser.get('photos_alignment', 'generic_preselection')
+            if generic_preselection == "True":
+                photos_alignment_generic_preselection = True
+            elif generic_preselection == "False":
+                photos_alignment_generic_preselection = False
+            else:
+                photos_alignment_generic_preselection = True
+                print(
+                    "Photos alignment generic_preselection option doesn't found in config file. Default setting will be used (True).")
+        except NoOptionError:
+            photos_alignment_generic_preselection = True
+            print("Photos alignment generic_preselection option doesn't found in config file. Default setting will be used (True).")
+
+        print("Photos alignment generic_preselection loaded: {}".format(str(photos_alignment_generic_preselection)))
+
+        try:
+            photos_alignment_key_point_limit = int(cfg_parser.get('photos_alignment', 'key_point_limit'))
+        except NoOptionError:
+            photos_alignment_key_point_limit = 40000
+            print("Photos alignment key_point_limit option doesn't found in config file. Default setting will be used (40000).")
+        except ValueError:
+            photos_alignment_key_point_limit = 40000
+            print("Photos alignment key_point_limit bad format. Default setting will be used (40000).")
+        print("Photos alignment key_point_limit loaded: {}".format(str(photos_alignment_key_point_limit)))
+
+        try:
+            photos_alignment_tie_point_limit = int(cfg_parser.get('photos_alignment', 'tie_point_limit'))
+        except NoOptionError:
+            photos_alignment_tie_point_limit = 4000
+            print("Photos alignment tie_point_limit option doesn't found in config file. Default setting will be used (4000).")
+        except ValueError:
+            photos_alignment_tie_point_limit = 4000
+        print("Photos alignment tie_point_limit loaded: {}".format(str(photos_alignment_tie_point_limit)))
+
+        # GENERAL section (this values  need to be loaded from the config file...)
+        self.project_name = project_name
+        self.working_directory = working_directory
+        self.project_directory = project_directory
+        self.log_path = log_path
+        self.images_directory = images_directory
+        self.mask_path = mask_path
+
+        # photos alignment section
+        self.photos_alignment_accuracy = photos_alignment_accuracy
+        self.photos_alignment_preselection = photos_alignment_preselection
+        self.photos_alignment_generic_preselection = photos_alignment_generic_preselection
+        self.photos_alignment_key_point_limit = photos_alignment_key_point_limit
+        self.photos_alignment_tie_point_limit = photos_alignment_tie_point_limit
+
+    def LoadDefaultConfig(self):
+        # GENERAL section (this values  need to be loaded from the config file...)
+        self.project_name = ""
+        self.working_directory = ""
+        self.project_directory = ""
+        self.log_path = ""
+        self.images_directory = ""
+        self.mask_path = ""
+
+        # photos alignment section
+        self.photos_alignment_accuracy = PhotoScan.MediumAccuracy
+        self.photos_alignment_preselection = PhotoScan.NoPreselection
+        self.photos_alignment_generic_preselection = True
+        self.photos_alignment_key_point_limit = 40000
+        self.photos_alignment_tie_point_limit = 4000
--- photoscan_script.py
+++ photoscan_script.py
@@ -3,7 +3,7 @@ import argparse
 import glob
 import os
 
-from configparser import ConfigParser, NoOptionError, NoSectionError
+from PhotoScanConfig import Configuration
 
 # Load config using python argparse if the config file is available
 config_file_path = ""
@@ -24,234 +24,7 @@ else:
     app.messageBox("Invalid config file path: \"{}\"".format(config_file_path))
     print("Invalid config file path: \"{}\"".format(config_file_path))
 
-# try parse config file
-cfg_parser = ConfigParser()
-cfg_parser.read(config_file_path)
-
-## photoscan_cfg = PhotoScanConfig.PSConfigurator(config_file_path)
-
-# load config file section
-# GENERAL section
-print("Loading config file...")
-project_name = cfg_parser.get('general', 'project_name')
-print("Project name configuration successfully loaded: {}".format(project_name))
-try:
-    working_directory = cfg_parser.get('general', 'working_directory')
-    if not os.path.exists(working_directory):
-        raise IOError("Path {} does not exist".format(working_directory))
-    print("Working directory configuration successfully loaded: {}".format(working_directory))
-except NoOptionError as ex:
-    app.messageBox("working_directory option is missing in the configuration file.")
-    raise ex
-except NoSectionError as ex:
-    app.messageBox("General section is missing in the configuration file.")
-    raise ex
-
-try:
-    project_directory = os.path.join(working_directory, cfg_parser.get('general', 'project_directory'))
-    if not os.path.exists(project_directory):
-        print("Project directory {} doesn't exist. Creating new one...". format(project_directory))
-        os.mkdir(project_directory)
-    print("Project directory configuration successfully loaded: {}".format(project_directory))
-except NoOptionError as ex:
-    app.messageBox("project_directory option is missing in the configuration file.")
-    raise ex
-except NoSectionError as ex:
-    app.messageBox("General section is missing in the configuration file.")
-    raise ex
-
-try:
-    log_path = os.path.join(working_directory, cfg_parser.get('general', 'log_path'))
-    if not os.path.exists(log_path):
-        raise IOError("Path {} does not exist".format(log_path))
-    print("Logs path configuration successfully loaded: {}".format(log_path))
-except NoOptionError as ex:
-    app.messageBox("log_path option is missing in the configuration file.")
-    raise ex
-except NoSectionError as ex:
-    app.messageBox("General section is missing in the configuration file.")
-    raise ex
-
-try:
-    images_directory = os.path.join(working_directory, cfg_parser.get('general', 'images_directory'))
-    if not os.path.exists(images_directory):
-        raise IOError("Path {} does not exist".format(images_directory))
-    print("Photos directory configuration successfully loaded: {}".format(images_directory))
-except NoOptionError as ex:
-    app.messageBox("images_directory option is missing in the configuration file.")
-    raise ex
-except NoSectionError as ex:
-    app.messageBox("General section is missing in the configuration file.")
-    raise ex
-
-try:
-    mask_path = os.path.join(working_directory, cfg_parser.get('general', 'mask_path'))
-    if not os.path.exists(mask_path):
-        raise IOError("Path {} does not exist".format(mask_path))
-    print("Mask path configuration successfully loaded: {}".format(mask_path))
-    if not os.path.exists(images_directory):
-        raise IOError("Path {} does not exist".format(images_directory))
-    print("Photos directory configuration successfully loaded: {}".format(images_directory))
-except NoOptionError as ex:
-    app.messageBox("mask_path option is missing in the configuration file.")
-    raise ex
-except NoSectionError as ex:
-    app.messageBox("General section is missing in the configuration file.")
-    raise ex
-
-# photos alignment section
-try:
-    accuracy = cfg_parser.get('photos_alignment', 'accuracy')
-    if accuracy == "LowestAccuracy":
-        photos_alignment_accuracy = PhotoScan.LowestAccuracy
-    elif accuracy == "LowAccuracy":
-        photos_alignment_accuracy = PhotoScan.LowAccuracy
-    elif accuracy == "MediumAccuracy":
-        photos_alignment_accuracy = PhotoScan.MediumAccuracy
-    elif accuracy == "HighAccuracy":
-        photos_alignment_accuracy = PhotoScan.HighAccuracy
-    elif accuracy == "HighestAccuracy":
-        photos_alignment_accuracy = PhotoScan.HighestAccuracy
-    else:
-        photos_alignment_accuracy = PhotoScan.MediumAccuracy
-        print("Photos alignment accuracy option doesn't found in config file. Default setting will be used (MediumAccuracy).")
-except NoOptionError:
-    photos_alignment_accuracy = PhotoScan.MediumAccuracy
-    print("Photos alignment accuracy option doesn't found in config file. Default setting will be used (MediumAccuracy).")
-
-except NoSectionError:
-    photos_alignment_accuracy = PhotoScan.MediumAccuracy
-    print(
-        "Photos alignment section doesn't found in config file.")
-    app.messageBox("Config file loading error. photos_alignment section is missing.")
-    raise IOError("Config file error.")
-print("Photos alignment accuracy loaded: {}".format(str(photos_alignment_accuracy)))
-
-try:
-    preselection = cfg_parser.get('photos_alignment', 'preselection')
-    if preselection == "NoPreselection":
-        photos_alignment_preselection = PhotoScan.NoPreselection
-    elif preselection == "GenericPreselection":
-        photos_alignment_preselection = PhotoScan.GenericPreselection
-    elif preselection == "ReferencePreselection":
-        photos_alignment_preselection = PhotoScan.ReferencePreselection
-    else:
-        photos_alignment_preselection = PhotoScan.NoPreselection
-        print("Photos alignment preselection option doesn't found in config file. Default setting will be used (NoPreselection).")
-except NoOptionError:
-    photos_alignment_preselection = PhotoScan.NoPreselection
-    print("Photos alignment preselection option doesn't found in config file. Default setting will be used (NoPreselection).")
-
-except NoSectionError:
-    print(
-        "Photos alignment section doesn't found in config file.")
-    app.messageBox("Config file loading error. photos_alignment section is missing.")
-    raise IOError("Config file error.")
-print("Photos alignment preselection loaded: {}".format(str(photos_alignment_preselection)))
-
-try:
-    generic_preselection = cfg_parser.get('photos_alignment', 'generic_preselection')
-    if generic_preselection == "True":
-        photos_alignment_generic_preselection = True
-    elif generic_preselection == "False":
-        photos_alignment_generic_preselection = False
-    else:
-        photos_alignment_generic_preselection = True
-        print("Photos alignment generic_preselection option doesn't found in config file. Default setting will be used (True).")
-except NoOptionError:
-    photos_alignment_generic_preselection = True
-    print("Photos alignment generic_preselection option doesn't found in config file. Default setting will be used (True).")
-
-except NoSectionError:
-    print(
-        "Photos alignment section doesn't found in config file.")
-    app.messageBox("Config file loading error. photos_alignment section is missing.")
-    raise IOError("Config file error.")
-print("Photos alignment generic_preselection loaded: {}".format(str(photos_alignment_generic_preselection)))
-
-try:
-    photos_alignment_key_point_limit = int(cfg_parser.get('photos_alignment', 'key_point_limit'))
-except NoOptionError:
-    photos_alignment_key_point_limit = 40000
-    print("Photos alignment key_point_limit option doesn't found in config file. Default setting will be used (40000).")
-except NoSectionError:
-    print(
-        "Photos alignment section doesn't found in config file.")
-    app.messageBox("Config file loading error. photos_alignment section is missing.")
-    raise IOError("Config file error.")
-except ValueError:
-    photos_alignment_key_point_limit = 40000
-    print("Photos alignment key_point_limit bad format. Default setting will be used (40000).")
-print("Photos alignment key_point_limit loaded: {}".format(str(photos_alignment_key_point_limit)))
-
-try:
-    photos_alignment_tie_point_limit = int(cfg_parser.get('photos_alignment', 'tie_point_limit'))
-except NoOptionError:
-    photos_alignment_tie_point_limit = 4000
-    print("Photos alignment tie_point_limit option doesn't found in config file. Default setting will be used (4000).")
-except NoSectionError:
-    print(
-        "Photos alignment section doesn't found in config file.")
-    app.messageBox("Config file loading error. photos_alignment section is missing.")
-    raise IOError("Config file error.")
-except ValueError:
-    photos_alignment_tie_point_limit = 4000
-print("Photos alignment tie_point_limit loaded: {}".format(str(photos_alignment_tie_point_limit)))
-
-
-# dense clud section
-try:
-    quality = cfg_parser.get("dense_cloud", "quality")
-    if quality == "UltraHigh":
-        dense_cloud_quality = PhotoScan.UltraQuality
-    elif quality == "High":
-        dense_cloud_quality = PhotoScan.HighQuality
-    elif quality == "Medium":
-        dense_cloud_quality = PhotoScan.MediumQuality
-    elif quality == "Low":
-        dense_cloud_quality = PhotoScan.LowQuality
-    elif quality == "Lowest":
-        dense_cloud_quality = PhotoScan.LowestQuality
-    else:
-        dense_cloud_quality = PhotoScan.MediumQuality
-        print("Wrong Dense cloud quality option. Default setting will be used (Medium).")
-except NoOptionError:
-    dense_cloud_quality = PhotoScan.MediumQuality
-    print("Dense cloud quality option doesn't found in config file. Default setting will be used (Medium).")
-except NoSectionError:
-    dense_cloud_quality = PhotoScan.MediumQuality
-    print("Dense cloud section doesn't found in config file.")
-    app.messageBox("Config file loading error. dense_cloud section is missing.")
-    raise IOError("Config file error.")
-finally:
-    print("dense could quality loaded: {}", format(str(dense_cloud_quality)))
-
-try:
-    filtering = cfg_parser.get("dense_cloud", "depth_filtering")
-    if filtering == "Disabled":
-        dense_cloud_filtering = PhotoScan.NoFiltering
-    elif filtering == "Mild":
-        dense_cloud_filtering = PhotoScan.MildFiltering
-    elif filtering == "Moderate":
-        dense_cloud_filtering = PhotoScan.ModerateFiltering
-    elif filtering == "Aggressive":
-        dense_cloud_filtering = PhotoScan.AggressiveFiltering
-    else:
-        dense_cloud_filtering = PhotoScan.NoFiltering
-        print("Wrong Dense cloud depth filtering option. Default setting will be used (Disabled).")
-except NoOptionError:
-    dense_cloud_filtering = PhotoScan.NoFiltering
-    print("Dense cloud depth filtering option doesn't found in config file. Default setting will be used (Disabled).")
-except NoSectionError:
-    dense_cloud_filtering = PhotoScan.NoFiltering
-    print("Dense cloud section doesn't found in config file.")
-    app.messageBox("Config file loading error. dense_cloud section is missing.")
-    raise IOError("Config file error.")
-finally:
-    print("dense could depth filtering loaded: {}", format(str(dense_cloud_filtering)))
-
-
-print("Configuration file successfully loaded.")
+config = Configuration(config_file_path=config_file_path)
 
 # create document and chunk
 doc = Document()
@@ -263,22 +36,22 @@ images = []
 image_extensions = ['*.jpg', '*.png', '*.raw']
 print("Loading images...")
 for image_extension in image_extensions:
-    for filename in glob.glob(os.path.join(images_directory, image_extension)):
+    for filename in glob.glob(os.path.join(config.images_directory, image_extension)):
         images.append(filename)
         print("Image {} loaded.".format(filename))
 
 if len(images) == 0:
     app.messageBox("No images found.")
-    print("No images found in directory {}".format(photos_directory))
+    print("No images found in directory {}".format(config.photos_directory))
 
 chunk.addPhotos(images)
 
 # load coordinates from the log file and add them into the chunk
 print("Loading references...")
 # check extension
-if os.path.split(log_path)[1] in ['csv', 'txt']:
+if os.path.split(config.log_path)[1] in ['csv', 'txt']:
     raise IOError("Log file {} has bad format. (Required csv or txt)")
-chunk.loadReference(log_path, columns='nyxz', delimiter='\t')
+chunk.loadReference(config.log_path, columns='nyxz', delimiter='\t')
 
 # set coordinates system
 print("Setting coordinate system on EPSG::4326...")
@@ -294,7 +67,7 @@ print("Updating transformation...")
 chunk.updateTransform()
 
 print("Photos alignment...")
-chunk.matchPhotos(accuracy=photos_alignment_accuracy,
+chunk.matchPhotos(accuracy=config.photos_alignment_accuracy,
                   preselection=photos_alignment_preselection,
                   generic_preselection=photos_alignment_generic_preselection,
                   keypoint_limit=photos_alignment_key_point_limit,
