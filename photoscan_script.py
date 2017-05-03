5263f8abb38e140678743324c9181e742a71d660
petrchmelar179 <petrchmelar179@gmail.com>
2017-05-03 14:55:19 +0200

    Implemented logging messages in script file.

--- photoscan_script.py
+++ photoscan_script.py
@@ -15,7 +15,8 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # file handler
-file_handler = logging.FileHandler('{}/script.log'.format(os.path.dirname(os.path.abspath(__file__))))
+file_handler = logging.FileHandler('{}/script.log'.format(os.path.dirname(os.path.abspath(__file__))),
+'w')
 file_handler.setFormatter(formatter)
 # stdout handler
 stdout_handler = logging.StreamHandler(sys.stdout)
@@ -48,48 +49,50 @@ config = Configuration(config_file_path=config_file_path)
 # create document and chunk
 doc = Document()
 chunk = doc.addChunk()
-print("Document and chunk created.")
+logger.info("Document and chunk created.")
 
 # mask
 chunk.importMasks(config.mask_path)
-print("Mask imported.")
+logger.info("Mask imported.")
 
 # load photos and add them into the chunk
 images = []
 image_extensions = ['*.jpg', '*.png', '*.raw']
-print("Loading images...")
+logger.info("Loading images...")
 for image_extension in image_extensions:
     for filename in glob.glob(os.path.join(config.images_directory, image_extension)):
         images.append(filename)
-        print("Image {} loaded.".format(filename))
+        logger.info("Image {} loaded.".format(filename))
 
 if len(images) == 0:
     app.messageBox("No images found.")
-    print("No images found in directory {}".format(config.photos_directory))
+    logger.warning("No images found in directory {}".format(config.photos_directory))
 
 chunk.addPhotos(images)
 
 # load coordinates from the log file and add them into the chunk
-print("Loading references...")
+logger.info("Loading references...")
 # check extension
 if os.path.split(config.log_path)[1] in ['csv', 'txt']:
-    raise IOError("Log file {} has bad format. (Required csv or txt)")
+    message = "Log file {} has bad format. (Required csv or txt)".format(config.log_path)
+    logger.error(message)
+    raise IOError(message)
 chunk.loadReference(config.log_path, columns='nyxz', delimiter='\t')
 
 # set coordinates system
-print("Setting coordinate system on EPSG::4326...")
+logger.info("Setting coordinate system on EPSG::4326...")
 chunk.crs = PhotoScan.CoordinateSystem("EPSG::4326")
 
 # set camera accuracy
-print("Setting camera location accuracy on {}...".format([0.05, 0.05, 0.05]))
+logger.info("Setting camera location accuracy on {}...".format([0.05, 0.05, 0.05]))
 chunk.camera_location_accuracy = PhotoScan.Vector((0.05, 0.05, 0.05))
 
 # update transformation
-print("Updating transformation...")
+logger.info("Updating transformation...")
 chunk.updateTransform()
 
 # photos alignment
-print("Photos alignment...")
+logger.info("Photos alignment...")
 chunk.matchPhotos(accuracy=config.photos_alignment_accuracy,
                   preselection=config.photos_alignment_preselection,
                   generic_preselection=config.photos_alignment_generic_preselection,
@@ -98,21 +101,21 @@ chunk.matchPhotos(accuracy=config.photos_alignment_accuracy,
 chunk.alignCameras()
 
 # build dense cloud
-print("Building dense cloud...")
+logger.info("Building dense cloud...")
 chunk.buildDenseCloud(quality=config.dense_cloud_quality,
                       filter=config.dense_cloud_filtering,
                       keep_depth = config.dense_cloud_keep_depth,
                       reuse_depth = config.dense_cloud_reuse_depth)
 
 # build mesh
-print("Building mesh...")
+logger.info("Building mesh...")
 chunk.buildModel(surface = config.mesh_surface,
                  source = PhotoScan.DenseCloudData,
                  interpolation = config.mesh_interpolation,
                  face_count = config.mesh_face_count)
 
 # build texture
-print("Building texture...")
+logger.info("Building texture...")
 chunk.buildUV(mapping=config.texture_mapping,
               count=config.texture_count)
 chunk.buildTexture(blending=config.texture_blending,
@@ -132,6 +135,10 @@ try:
   exporter = PhotoScanExporter(config, chunk)
   exporter.exportAll()
 except ValueError as err:
-  raise IOError("PhotoScanExporter: " + err.args)
+    message = "PhotoScanExporter: " + err.args
+    logger.error(message)
+    raise IOError(message)
 
 doc.save(path=os.path.join(config.project_directory, config.project_name + '.psx'))
+
+logger.info("Project saved.")
